e1f0664899f35a42052c212586d02084b7f1cfc1
laulloliv <laulloliv@gmail.com>
2021-08-25 20:12:15 -0400

    Funções de calculo adicionadas

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ function clean(){
   document.getElementById('output').innerHTML = "";
 }
 
+function back(){
+  var output = document.getElementById('output').innerHTML;
+  document.getElementById('output').innerHTML = output.substring(0, output.length -1);
+}
 
+function calcular(){
+  var output = document.getElementById('output').innerHTML;
+  if(output){
+    document.getElementById('output').innerHTML = eval(output);
+  }else{
+    document.getElementById('output').innerHTML = "Tente calcular";
+  }
+}
 
